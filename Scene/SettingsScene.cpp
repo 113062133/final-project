85f9859e0e1701afc506fa9f5fc16aca8ddb1492
LittleGreenMan777 <pengchi777@gmail.com>
2025-06-13 23:28:53 +0800

    change bgm

--- Scene/SettingsScene.cpp
+++ Scene/SettingsScene.cpp
@@ -37,7 +37,7 @@ void SettingsScene::Initialize() {
     AddNewControlObject(sliderSFX);
     AddNewObject(new Engine::Label("SFX: ", "pirulen.ttf", 28, 40 + halfW - 60 - 95, halfH + 50, 255, 255, 255, 255, 0.5, 0.5));
     // Not safe if release resource while playing, however we only free while change scene, so it's fine.
-    bgmInstance = AudioHelper::PlaySample("select.ogg", true, AudioHelper::BGMVolume);
+    bgmInstance = AudioHelper::PlaySample("play.ogg", true, AudioHelper::BGMVolume);
     sliderBGM->SetValue(AudioHelper::BGMVolume);
     sliderSFX->SetValue(AudioHelper::SFXVolume);
 }
